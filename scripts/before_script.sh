c025e8067c999a933ec6d0f6d860417883545134
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2022-05-02 15:54:16 +0530

    Update and rename before-script.sh to before_script.sh

---
+++ scripts/before_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+echo "** (sample-nodejs)-api-prod process status **" >> /tmp/(samplenodejs)-api-prod_deploy_logs
+runuser -l ubuntu -c 'sudo pm2 status' | grep -wo nodejs
+if [  $? -ne 0 ];
+then
+   echo "############################## pm2 not running #################################" >> /tmp/(sample-nodejs)-api-prod_deploy_logs
+else
+   echo "############################## pm2 already running Deleting ####################" >> /tmp/(sample-nodejs)-api-prod_deploy_logs
+    runuser -l ubuntu -c 'sudo pm2 delete nodejs'
+fi
+
+rm -rf /home/ubuntu/(sample-nodejs)
+
+if [ ! -d /home/ubuntu/(sample-nodejs) ]; then
+runuser -l ubuntu -c 'mkdir -p /home/ubuntu/(sample-nodejs)' >> /tmp/(sample-nodejs)-prod_deploy_logs
+fi
